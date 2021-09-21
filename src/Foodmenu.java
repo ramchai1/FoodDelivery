78993ae2cbf74ec249c65e13486c4fbfd5060d24
Ramesh Ramakrishna <ram.events1@gmail.com>
2021-09-21 10:52:32 +0530

    Food updates1

--- src/Foodmenu.java
+++ src/Foodmenu.java
@@ -1,5 +1,6 @@
 public class Foodmenu {
     public static void main(String[] args) {
         System.out.println("Welcome to food world !");
+        System.out.println("Food will start from monday");
     }
 }
